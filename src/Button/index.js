ae3379ef5cc856bc500a8515096a0d43aa614b44
jdu <jdu.xian@gmail.com>
2017-06-09 17:27:48 +0800

    Adding a Stylesheet

---
+++ src/Button/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './index.css';
+
+class Button extends Component {
+  render() {
+    return <div className="Button">确定</div>;
+  }
+}
+
+export default Button;
